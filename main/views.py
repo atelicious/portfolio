8321c87496dec320b0aeb08d11b6b7f3f352d8e8
atelicious <elhytablazon@yahoo.com.ph>
2022-11-02 01:48:46 +0800

    updated portfolio project

--- main/views.py
+++ main/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 
 def home(request):
+    user = User.objects.get(first_name='Elhy')
+    categories = Category.objects.all()
+    certificates = Certificate.objects.all()
+
     context = {
         'title': 'Welcome',
+        'user' :user,
+        'categories': categories,
+        'certificates': certificates
     }
-    return render(request, 'main/home.html', context)
 
+    return render(request, 'main/home.html', context)
 
